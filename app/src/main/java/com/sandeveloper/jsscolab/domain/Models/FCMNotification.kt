032a3a2b3368c106f20880d3afc77f91c83e6b1a
SANKALP SAXENA <sankalpsaxena2004@gmail.com>
2024-12-14 09:25:52 +0530

    Fcm Notification

--- app/src/main/java/com/sandeveloper/jsscolab/domain/Models/FCMNotification.kt
+++ app/src/main/java/com/sandeveloper/jsscolab/domain/Models/FCMNotification.kt
@@ -5,7 +5,7 @@ import com.sandeveloper.jsscolab.domain.Constants.NotificationType
 data class FCMNotification constructor(
     val notificationType: NotificationType,
     val title:String = "",
-    val desc: String = "",
-    val message: String = "",
-    val address:String? = null
+    val body: String = "",
+//    val message: String = "",
+//    val address:String? = null
 )
